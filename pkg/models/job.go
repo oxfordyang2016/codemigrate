9529e2403abca9d3d0e1c8eb94d5881c969ce364
yangming <yang756260386@gmail.com>
2017-01-18 18:25:23 +0800

    allok

--- pkg/models/job.go
+++ pkg/models/job.go
@@ -227,8 +227,8 @@ func DB() *xorm.Engine {
 
 
     		`)
-//`		sess = sess.Limit(p.PageSize, (p.PageNum-1)*p.PageSize)
-    	sess = sess.Limit(3,3)//count ,offset
+        sess = sess.Limit(p.PageSize, (p.PageNum-1)*p.PageSize)
+    	//sess = sess.Limit(3,3)//count ,offset
 
 	} 
 	//this is quering jobs.
