204ecc6c749d944dd114ea7c2fc1171b66994787
yangming <yang756260386@gmail.com>
2017-01-18 19:30:02 +0800

    allok

--- pkg/models/job.go
+++ pkg/models/job.go
@@ -163,8 +163,8 @@ func DB() *xorm.Engine {
 
 
 		`,filter.Title)
-    //sess = sess.Where("package_pkg.title like ?", fmt.Sprintf("'%%%s%%'", filter.Title))
-  sess,_ = sess.Where("package_pkg.title=?", fmt.Sprintf("'%%%s%%'", filter.Title))
+    sess,err = sess.Where("package_pkg.title like ?", fmt.Sprintf("'%%%s%%'", filter.Title))
+  //sess= sess.Where("package_pkg.title=?", fmt.Sprintf("'%%%s%%'", filter.Title))
   //sess = sess.Exec("select * from package_pkg where package_pkg.title=123qa")
 	/*
 	if filter != nil {
