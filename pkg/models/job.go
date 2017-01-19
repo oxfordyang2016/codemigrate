6f03c3ff13c4e64b06588051b24942133c48af64
yangming <yang756260386@gmail.com>
2017-01-19 22:32:37 +0800

    allok

--- pkg/models/job.go
+++ pkg/models/job.go
@@ -232,13 +232,20 @@ if filter.Title != "" {
 		return nil, err
 	}
     return jobs, nil
-    }else{
+    }
 
+
+
+    	if p != nil {
+		n, _ := sess.Count(new(Job))
+		p.TotalNum = n
+		 sess = sess.Limit(p.PageSize, (p.PageNum-1)*p.PageSize)// it works
+	}
 	if err := sess.Find(&jobs); err != nil {
 		return nil, err
 	}
 	return jobs, nil 	
-    }
+    
 
 	/*if err := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).Or("package_job.uid = ?", filter.Owner).Find(&jobs); err != nil {
 		//if err := sess.Find(&jobs); err != nil {
