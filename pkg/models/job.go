5006a7bcf89eb39a4a65b4295d3092b6ccabaf0b
yangming <yang756260386@gmail.com>
2017-01-19 22:18:05 +0800

    allok

--- pkg/models/job.go
+++ pkg/models/job.go
@@ -230,9 +230,7 @@ if filter.Title != "" {
 		return nil, err
 	}}
 
- if err := sess.Find(&jobs); err != nil {
-		return nil, err
-	}
+ 
 
 
 	/*if err := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).Or("package_job.uid = ?", filter.Owner).Find(&jobs); err != nil {
