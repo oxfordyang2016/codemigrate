3ebb006c039815a4f67e0c5935095000db898a91
yangming <yang756260386@gmail.com>
2017-01-20 14:19:08 +0800

    allok

--- pkg/models/job.go
+++ pkg/models/job.go
@@ -190,9 +190,14 @@ func GetJobsEx(typ int, p *cydex.Pagination, filter *JobFilter) ([]*Job, error) 
 
 //if err := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).Or("package_job.uid = ?", filter.Owner).Find(&jobs); err != nil {
    if filter.Title != "" && (filter.Owner != ""&&typ == cydex.UPLOAD ){
-n, _ := sess.Count(new(Job))
-			fmt.Println(`count is all----------------------->`,n)
 
+//total, _ := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).And("package_job.uid = ?", filter.Owner)..Count(&jobs)
+if p != nil {
+		n, _ := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).And("package_job.uid = ?", filter.Owner)..Count(&jobs)
+		p.TotalNum = n
+		sess = sess.Limit(p.PageSize, (p.PageNum-1)*p.PageSize)// it works
+	}
+		
 
 if err := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).And("package_job.uid = ?", filter.Owner).Find(&jobs); err != nil {
 		//if err := sess.Find(&jobs); err != nil {
@@ -205,8 +210,14 @@ if err := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid")
    }
 
 	if filter.Title != "" || (filter.Owner != "" && typ == cydex.UPLOAD){
-		n, _ := sess.Count(new(Job))
-			fmt.Println(`count is all----------------------->`,n)
+if p != nil {
+		n, _ := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).Or("package_job.uid = ?", filter.Owner).Count(&jobs)
+		p.TotalNum = n
+		sess = sess.Limit(p.PageSize, (p.PageNum-1)*p.PageSize)// it works
+	}
+		
+
+
 	    if err := sess.Join("INNER", "package_pkg", "package_pkg.pid = package_job.pid").Where("package_pkg.title=?",filter.Title).Or("package_job.uid = ?", filter.Owner).Find(&jobs); err != nil {
 		//if err := sess.Find(&jobs); err != nil {
 		
