eace9fe6c2fc227925d3844185c3f7e49545a314
yangming <yang756260386@gmail.com>
2017-01-20 10:10:40 +0800

    allok

--- pkg/models/job.go
+++ pkg/models/job.go
@@ -253,11 +253,11 @@ if filter.Title != "" {
 	}
 	*/
 	
-/*
+
 	if !has_orderby {
 		sess = sess.Desc("package_job.create_at")
 	}
-*/
+
 	if p != nil {
 		n, _ := sess.Count(new(Job))
 		p.TotalNum = n
